8e14a0c2c6b568ebc1792702537357c0767819c3
howard.yu <howard.yu@gigabyte.com>
2015-06-10 17:26:54 +0800

    add modify/del page and  create page

--- BabyBook/app/controllers/baby_controller.rb
+++ BabyBook/app/controllers/baby_controller.rb
@@ -8,8 +8,32 @@ class BabyController < ApplicationController
   end
 
   def create
-  	@bpook = Bpook.new(parmar[:bpook])
+  	@bpook = Bpook.new(bpook_params)
   	@bpook.save
+  	
   	redirect_to(:action => "index")
+  end
+
+  def modify
+  	@bpook = Bpook.find(params[:id])
+  end
+
+  def updateing
+  	@bpook = Bpook.find(params[:id])
+  	@bpook.update_attributes(bpook_params)
+
+  	redirect_to(:action => "index")
+  end
+  def del
+	@bpook = Bpook.find(params[:id])
+	@bpook.destroy
+
+	redirect_to(:action => "index")
+  end
+
+  private
+  
+  def bpook_params
+	params.require(:bpook).permit(:name, :years, :birthday)
   end
 end
